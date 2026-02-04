a963601d92789865a7d2f3c93f859b40167028bb
rumbus-dev <ysc@rumbus.kr>
2026-02-04 20:28:24 +0900

    상단 메뉴바 고정되도록 수정

--- script.js
+++ script.js
@@ -17,4 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
 			}
 		});
 	});
+
+	// Header Scroll Effect
+	const header = document.querySelector("header");
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 50) {
+			header.classList.add("scrolled");
+		} else {
+			header.classList.remove("scrolled");
+		}
+	});
 });
